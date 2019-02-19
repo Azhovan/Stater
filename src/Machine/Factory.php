6eb9e6096cdcf299990eda3c311f42cb86a34657
azhovan asadi <elazhi@outlook.com>
2019-02-19 15:07:07 +0330

    refactor factory class

--- src/Machine/Factory.php
+++ src/Machine/Factory.php
@@ -4,66 +4,151 @@ declare(strict_types=1);
 
 namespace Stater\Machine;
 
+use Stater\Events\Factory as EventFactory;
+use Stater\States\Factory as StateFactory;
+
 class Factory
 {
-    /**
-     * Create state machine with inputs
-     *
-     * $machine::create([
-     *   [
-     *       "state" => ["name" =>"state1", "data" => []],
-     *       "event" => ["name" =>"event1", "data" => []],
-     *       "condition" => closure(),
-     *       "callback" => closure(),
-     *       "transitionTo" => ["name" =>"state2", "data" => []]
-     *   ]
-     *
-     * ]);
-     *
-     * @param  array $entries
-     * @return mixed
-     */
-    public static function create(?array $entries)
-    {
-        $machine = new StateMachine(null);
+	/**
+	 * Hold all states in the state machine
+	 *
+	 * @var array
+	 */
+	private $states = [];
 
-        if (null === $entries || empty($entries)) {
-            return $machine;
-        }
+	/**
+	 * Holds all events in the state machine
+	 *
+	 * @var array
+	 */
+	private $events = [];
 
-        foreach ($entries as $entry) {
+	/**
+	 * @var StateMachine
+	 */
+	private $machine;
 
-            $states = (new \Stater\States\Factory())
-                ->create(
-                    [
-                    $entry["state"],
-                    $entry["transitionTo"]
-                    ]
-                );
+	/**
+	 * Factory constructor.
+	 *
+	 * @param StateMachine $machine
+	 */
+	public function __construct (StateMachine $machine)
+	{
+		$this->machine = $machine ?? new StateMachine(null);
+	}
 
-            $event = (new \Stater\Events\Factory())
-                ->create(
-                    [
-                    $entry["event"]
-                    ]
-                );
+	/**
+	 * Create state machine with inputs
+	 *
+	 * @param  array $entries
+	 *
+	 * @return mixed
+	 */
+	public function create (?array $entries)
+	{
 
-            $machine
-                ->state(
-                    $states($entry["state"]["name"])
-                )
-                ->on(
-                    $event($entry["event"]["name"]),
-                    $entry["condition"]
-                )
-                ->transitionTo(
-                    $states($entry["transitionTo"]["name"]),
-                    $entry["callback"]
-                )
-                ->get();
-        }
+		if (null === $entries || empty($entries)) {
+			return $this->machine;
+		}
 
-        return $machine;
-    }
+		foreach ($entries as $entry) {
+
+			$states = $this->states($entry["state"], $entry["transitionTo"]);
+			$event = $this->events($entry["event"]);
+
+			$this->make($entry, $states, $event);
+		}
+
+	}
+
+	private function make ($entry, $states, $event)
+	{
+		$this->machine
+			->state(
+				$states($entry["state"]["name"])
+			)
+			->on(
+				$event($entry["event"]["name"]),
+				$entry["condition"]
+			)
+			->transitionTo(
+				$states($entry["transitionTo"]["name"]),
+				$entry["callback"]
+			);
+	}
+
+	/**
+	 * @param $entry
+	 *
+	 * @return mixed
+	 */
+	private function states (...$entry)
+	{
+		$states = [];
+
+		foreach ($entry as $item) {
+			if (is_string($item)) {
+				if (array_key_exists($item, $this->states)) {
+					continue;
+				} else {
+					$states[] = $item;
+				}
+			}
+
+			if (is_array($item)) {
+				if (array_key_exists($item['name'], $this->states)) {
+					continue;
+				} else {
+					$states[] = $item;
+					$this->states['name'] = $item;
+				}
+			}
+		}
+
+		return (new StateFactory())->create($states);
+	}
+
+	/**
+	 * @param $entry
+	 *
+	 * @return mixed
+	 */
+	private function events (...$entry)
+	{
+		$events = [];
+
+		foreach ($entry as $item) {
+			if (is_string($item)) {
+				if (array_key_exists($item, $this->$events)) {
+					continue;
+				} else {
+					$events[] = $item;
+				}
+			}
+
+			if (is_array($item)) {
+				if (array_key_exists($item['name'], $this->$events)) {
+					continue;
+				} else {
+					$events[] = $item;
+					$this->events['name'] = $item;
+				}
+			}
+		}
+
+		return (new EventFactory())->create($events);
+	}
+
+	/**
+	 * Return the state machine map
+	 *
+	 * @return array
+	 */
+	public function get()
+	{
+		return $this->machine->get();
+	}
+
 
 }
